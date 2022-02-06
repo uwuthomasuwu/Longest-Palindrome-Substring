c64baaf8aa2ab8a5f49b99461fe53649d9fedddf
spacemoon <Thomaschung1011@gmail.com>
2022-02-07 04:02:02 +0900

    palindrome expanding from middle solved

--- LPS.py
+++ LPS.py
@@ -1,29 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        ans = 0
-        start = 0
-        end = 0
+        start, end = 1, 0
+        if n <= 1:
+            return s
+
         for i in range(n):
-            one = self.expand_from_mid(s, i, i)
-            two = self.expand_from_mid(s, i, i+1)
-            longest = one if one >= two else two
+            one = self.expand_from_mid(s, i, i,n)
+            two = self.expand_from_mid(s, i, i+1,n)
+            longest = one if one > two else two
             if longest > end-start + 1:
-                start, end = 3, 3
+                end = i+longest//2
+                start = end-longest+1
+        return print(s[start:end+1])
 
-        return s[start:end]
-
-    def expand_from_mid(self, astr, aleft, aright):
-        ans = 0
-        while True:
-            if aleft < 0 or aright >= len(astr):
+    def expand_from_mid(self, astr, aleft, aright,n):
+        while aleft>=0 and aright<n:
+            if astr[aleft] != astr[aright]:
                 break
-            elif astr[aleft] == astr[aright]:
-                ans += 1
-                aleft -= 1
-                aright += 1
-        return ans
+            aleft, aright = aleft-1,aright+1
+        return aright - aleft - 1
 
 mysol = Solution()
-inputt = "babad"
-mysol.longestPalindrome(inputt)
+n = input()
+mysol.longestPalindrome(n)
